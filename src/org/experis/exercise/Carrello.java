0b3af20128e7709edf8da413a077eb1140b6618c
luca-masera <lucamasera1995@gmail.com>
2024-04-02 15:40:04 +0200

    class Prodotto and Carrello

--- src/org/experis/exercise/Carrello.java
+++ src/org/experis/exercise/Carrello.java
@@ -1,24 +1,9 @@
 package org.experis.exercise;
 
 
-    public class Main {
+    public class Carrello {
         public static void main(String[] args) {
-            Prodotto prodotto1 = new Prodotto("Macchina","Mini cooper", 14000, 22);
-            Prodotto prodotto2 = new Prodotto("Tazza","Tazza orientale di marmo", 150,22);
 
-            //STAMPO prodotto1
-            System.out.println("Il mio primo prodotto è una:" + prodotto1.nomeEsteso());
-            System.out.println("Il modello del prodotto è:" + " " + prodotto1.marca);
-            System.out.println("Il prezzo base senza Iva è:" + prodotto1.price() + "$");
-            System.out.println("Il prezzo con Iva è:" + prodotto1.priceIva() + "$");
-
-            System.out.println();
-
-            //STAMPO prodotto2
-            System.out.println("Il mio secondo prodotto è una:" + prodotto2.nomeEsteso());
-            System.out.println("Il modello del prodotto è:" + " " + prodotto2.marca);
-            System.out.println("Il prezzo base senza Iva è:" + prodotto2.price() + "$");
-            System.out.println("Il prezzo con Iva è:" + prodotto2.priceIva() + "$");
         }
     }
 
--- src/org/experis/exercise/Prodotto.java
+++ src/org/experis/exercise/Prodotto.java
@@ -1,2 +1,81 @@
-package org.experis.exercise;public class Prodotto {
+package org.experis.exercise;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class Prodotto {
+
+    //ATTRIBUTI
+    private int codice;
+    private  String nome;
+    private String marca;
+    private double prezzo;
+    private double iva;
+
+    //COSTRUTTORI
+
+
+    public Prodotto(int codice, String nome, String marca, double prezzo, double iva) {
+        this.codice = codice;
+        this.nome = nome;
+        this.marca = marca;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    // GETTER
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    //SETTER
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    public double priceSenzaIva(){
+
+        return prezzo;
+    }
+    public double priceIva(){
+
+        return prezzo * (1 + iva / 100);
+    }
+    public String getDettagliProdottoFormattati() {
+        DecimalFormat df = new DecimalFormat("#.##");
+        return "Codice: " + codice +
+                "\nNome: " + nome +
+                "\nMarca: " + marca +
+                "\nPrezzo: " + df.format(priceSenzaIva()) +
+                "\nIVA: " + df.format(iva) + "%" +
+                "\nPrezzo con IVA: " + df.format(priceIva());
+    }
+
+
+
+
 }
